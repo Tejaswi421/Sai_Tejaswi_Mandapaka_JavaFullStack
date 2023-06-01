45bcf2835f968e5c4ae5af43c041ae87154edcdf
Tejaswi421 <59414141+Tejaswi421@users.noreply.github.com>
2023-06-01 01:45:36 -0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student
+{
+    String Name="Tejaswi";
+    public String getName()
+    {
+        return Name;
+    }
+    public String setName(String name)
+    {
+        Name=name;
+        return name;
+
+    }
+
+}
