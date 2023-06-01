a26274bc7e554130484db7fb7a0d16c1646a3707
Tejaswi421 <saitejaswi421@gmail.com>
2023-06-01 15:18:28 -0500

    Adding files

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        Student s = new Student();
-        System.out.println(s.getName());
-        System.out.println(s.setName("Divya"));
-    }
-}
